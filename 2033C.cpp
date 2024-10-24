bf62be2a0165912e66fc3f9cedcc83e0ce6428d1
Soham Chaudhuri <sohamchaudhuri.12.a.38@gmail.com>
2024-10-24 20:55:33 +0530

    contest 24.10.2024

--- 2033C.cpp
+++ 2033C.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
 typedef long double ld;
@@ -8,69 +8,58 @@ typedef vector<vector<lli>> vvlli;
 typedef vector<lli> vlli;
 typedef vector<ull> vull;
 typedef pair<lli, lli> plli;
-#define max3(a, b, c) max(max((a), (b)), (c))
-#define max4(a, b, c, d) max(max((a), (b)), max((c), (d)))
-#define min3(a, b, c) min(min((a), (b)), (c))
-#define min4(a, b, c, d) min(min((a), (b)), min((c), (d)))
-#define loop(i, a, b) for (int i = a; i <= b; i++)
-#define bloop(i, a, b) for (int i = a; i >= b; i--)
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+#define loop(i,a,b) for(int i=a;i<=b;i++)
+#define bloop(i,a,b) for(int i=a;i>=b;i--)
 #define pb push_back
 #define pop pop_back
 #define inf 9999999999999
 #define ninf -99999999999999
 #define endl '\n'
 #define setprecision(x) sp(x)
-#define all(x) x.begin(), x.end()
-#define pno cout << "NO" << endl;
-#define py cout << "YES" << endl;
+#define all(x) x.begin(),x.end()
+#define pno cout<<"NO"<<endl;
+#define py cout<<"YES"<<endl;
 const lli mod = 1e7 + 7;
 
 // Input vector
-void inall(vlli &v, lli n)
-{
-    loop(i, 0, n - 1)
-    {
+void inall(vlli &v, lli n) {
+    loop(i,0,n-1) {
         lli x;
         cin >> x;
         v.pb(x);
     }
 }
 
-lli gcd(lli a, lli b)
-{
-    if (b == 0)
-        return a;
+lli gcd(lli a, lli b) {
+    if (b == 0) return a;
     return gcd(b, a % b);
 }
 
-lli pwr(lli a, lli b)
-{
+lli pwr(lli a, lli b) {
     a %= mod;
     lli res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = res * a % mod;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
         a = a * a % mod;
         b >>= 1;
     }
     return res;
 }
 
-lli lcm(lli a, lli b)
-{
+lli lcm(lli a, lli b) {
     return (a / gcd(a, b)) * b;
 }
 
 // Sieve of Eratosthenes
 lli prime[mod];
-void SieveOfEratosthenes(int n)
-{
+void SieveOfEratosthenes(int n) {
     memset(prime, true, sizeof(prime));
-    for (int p = 2; p * p <= n; p++)
-    {
-        if (prime[p] == true)
-        {
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
             for (int i = p * p; i <= n; i += p)
                 prime[i] = false;
         }
@@ -84,40 +73,38 @@ void SieveOfEratosthenes(int n)
 // bitset <20> b(x); x = 50 or x = "110010"
 // lli y = b.to_ulong();
 
-vlli dp;
-int solver(int idx, vlli &a, vlli &b, vlli &vis)
-{
-    int n = a.size();
-    if (idx == n)
-        return -1e9;
-    if (dp[idx] != -1)
-        return dp[idx];
-    int ans = -1e9;
-    loop(i,0,n-1){
-
-    }
-}
-
-void f()
-{
+void f() {
     lli n;
-    vlli a, b;
+    cin>>n;
+    vlli a;
     inall(a, n);
-    inall(b, n);
-    dp = vlli(n + 1, -1);
-    vlli vis(n, 0);
-    cout << solver(0, a, b, vis) << endl;
+    loop(i,0,n-2){
+        if(a[i]==a[i+1]){
+            if(n-i-2>=0&&a[n-i-2]==a[i]) continue;
+            if(n-i+1<n&&a[n-i+1]==a[i]) continue;
+            swap(a[i],a[n-i-1]);
+        }
+    }
+    // loop(i,0,n-1){
+    //     cout<<a[i]<<" ";
+    // }
+    // cout<<endl;
+    lli ans=0;
+    loop(i,0,n-2){
+        if(a[i]==a[i+1]){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
 }
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         // YOUR CODE GOES HERE
         // PAY ATTENTION BEFORE TAKING UNSIGNED OR SIGNED
         f();
